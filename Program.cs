22c915412bdd8a93910ff2289369983ae50d42b6
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-05-17 17:31:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        private static ReadOnlySpan<char> str;
+        static void Main(string[] args)
+        {
+            string[] str = { "I used to eat drugs for ten years" };
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (ValidWordFound(str[i])) Console.WriteLine(str[i] + "*");
+            }
+            Console.ReadKey();
+        }
+        static bool ValidWordFound(string word)
+        {
+            return Regex.IsMatch(str, @"*");
+        }
+    }
+}
